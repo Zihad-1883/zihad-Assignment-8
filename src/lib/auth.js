e2a8c7ab32b26a228fd9e92667875c1f827986cb
zahinpilot2009-ai <your_github_email@gmail.com>
2026-05-03 13:49:19 +0600

    login and sign up page done

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,16 +1,27 @@
+import dns from 'node:dns';
+dns.setServers(['8.8.8.8', '8.8.4.4']);
+
 import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 
 const client = new MongoClient(process.env.MONGODB_URI);
-const db = client.db();
+const db = client.db("SkillSphere");
+
+// Add this to test connection
+client.connect()
+  .then(() => console.log("✅ MongoDB connected successfully"))
+  .catch((err) => console.error("❌ MongoDB connection failed:", err.message));
 
 export const auth = betterAuth({
+  secret: process.env.BETTER_AUTH_SECRET,
+  baseURL: process.env.BETTER_AUTH_URL,
+
   database: mongodbAdapter(db, {
-    // Optional: if you don't provide a client, database transactions won't be enabled.
     client
   }),
-    emailAndPassword: { 
-    enabled: true, 
-  }
+
+  emailAndPassword: {
+    enabled: true,
+  },
 });
